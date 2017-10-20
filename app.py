269c37c1d4d366525cf6458a508344e940828647
nmusaelian-rally <nmusaelian@rallydev.com>
2017-10-19 22:57:02 -0600

    commented out db.create_all()

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from models import Result
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost/kbdbf"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-db.create_all()
-db.session.commit()
-
-from models import Result
+# db.create_all()
+# db.session.commit()
 
 @app.route('/')
 def hello():
