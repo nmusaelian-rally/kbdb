bb30bf62849696d4bb5fe76afc1bd969f33e94fa
nmusaelian-rally <nmusaelian@rallydev.com>
2017-10-19 23:01:09 -0600

    db.create_all(), removed offending import

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from models import Result
+#from models import Result
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost/kbdbf"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# db.create_all()
-# db.session.commit()
+db.create_all()
+db.session.commit()
 
 @app.route('/')
 def hello():
