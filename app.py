5b5ee7370aa065a52c0c0ee209771d93bb5450ad
nmusaelian-rally <nmusaelian@rallydev.com>
2017-11-09 21:26:10 -0700

    added recession series to yield delta chart

--- app.py
+++ app.py
@@ -8,46 +8,21 @@ from bokeh.resources import INLINE
 from bokeh.util.string import encode_utf8
 from bokeh.models import ColumnDataSource, SingleIntervalTicker, LinearAxis
 from bokeh.models import HoverTool
+import pandas as pd
 import pandas_datareader.data as web
 from datetime import datetime
 
 
-# def connect_db():
-#     dburi = os.environ['DATABASE_URL']
-#     path = dburi.split('://')[1]
-#     user, password = path.split('@')[0].split(':')
-#     host, dbname = path.split('@')[1].split('/')
-#     host, port   = host.split(':')
-#     conn = psycopg2.connect(database=dbname, user=user, password=password, host=host, port=port)
-#     print("Opened database successfully")
-#     populateFromCSV(conn, 'snp500', 'data/snp500dump.csv')
-#     conn.close()
-#
-# def populateFromCSV(conn, table_name, csv_file):
-#     f = open(csv_file, 'r')
-#     next(f)
-#     conn.cursor().copy_from(f, table_name, columns=('date', 'sp500'), sep=',')
-#     conn.commit()
-#
-#
-#
-#
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
-# app.debug = True
-# connect_db()
-
-
 app = Flask(__name__)
 app.debug = True
+
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
 @app.route('/t-yield-delta')
-def bokeh():
+def buildChart():
     start = '2000-01-01'
     # we are going to match results with the dates of two latest recessions:
     # March-November 2001 Recession and December 2007 - June 2009  Recession
@@ -64,17 +39,27 @@ def bokeh():
 
 
     ds = ColumnDataSource(t_yields)
+
+    recessions = web.DataReader('USRECM', "fred", start, today)
+
+    ds2 = ColumnDataSource(recessions)
+
     title = "Yield difference between 10-Year and 3-Month Treasurys"
-    fig = figure(plot_width=800,
-                 plot_height=600,
+    fig = figure(plot_width   = 800,
+                 plot_height  = 600,
                  x_axis_label = "Date",
                  y_axis_label = "Delta",
-                 x_axis_type="datetime",
+                 x_axis_type  = "datetime",
                  title = title)
     fig.xaxis[0].ticker.desired_num_ticks = 10
-    fig.add_tools(HoverTool(tooltips=[("DATE", "@x{%F}"),("Treasurys 10YearRate - 3MonthRate", "@y{0.00 a}")],
-                            formatters={"x": "datetime"}))
-    fig.line(ds.data['DATE'],ds.data['Difference'], line_color="green", legend="delta")
+    fig.add_tools(HoverTool(tooltips=[("DATE", "@x{%F}"),("10Y Rate - 3M Rate", "@y{0.00 a}")],
+                            formatters={"x": "datetime"}, names=['rate-delta']))
+    fig.add_tools(HoverTool(tooltips=[("DATE", "@x{%F}")],
+                            formatters={"x": "datetime"}, names=['recession-dates']))
+    fig.line(ds.data['DATE'],ds.data['Difference'], line_color="green", legend="delta", name='rate-delta')
+    fig.line(ds2.data['DATE'], ds2.data['USRECM'],  line_color="red",   legend="recession", name='recession-dates')
+
+
     # grab the static resources
     js_resources = INLINE.render_js()
     css_resources = INLINE.render_css()
@@ -89,6 +74,7 @@ def bokeh():
         css_resources=css_resources,
     )
     return encode_utf8(html)
+
 
 if __name__ == '__main__':
     app.run()
