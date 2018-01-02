86236de963bca134174ef5a93fba6095913b0868
nmusaelian-rally <nmusaelian@rallydev.com>
2018-01-01 19:45:18 -0700

    models.py cleanup

--- models.py
+++ models.py
@@ -1,23 +1,4 @@
 from app import db
-import time
-from sqlalchemy.dialects.postgresql import JSON
-
-
-class Result(db.Model):
-    __tablename__ = 'results'
-
-    id  = db.Column(db.Integer, primary_key=True)
-    tag = db.Column(db.String(20),   unique=False, nullable=True)
-    text = db.Column(db.Text,        unique=False, nullable=True)
-    ref  = db.Column(db.String(120), unique=False, nullable=True)
-
-    def __init__(self,tag,text,ref):
-        self.tag  = tag
-        self.text = text
-        self.ref  = ref
-
-    def __repr__(self):
-        return '<Result %r>' % self.tag
 
 class SNP500(db.Model):
     __tablename__ = 'snp500'
@@ -27,8 +8,6 @@ class SNP500(db.Model):
     sp500  = db.Column(db.Float, unique=False, nullable=True)
 
     def __init__(self,date,sp500):
-        pattern = '%Y-%m-%d'
-        #self.date  = int(time.mktime(time.strptime(date, pattern)))
         self.date = date
         self.sp500 = sp500
 
